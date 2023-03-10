abdd586feadad094216150e16a774a02185bb1d7
PixelNeko <160456897+PxNeko@users.noreply.github.com>
2023-03-10 15:32:08 -0600

    Add filter before appending to history

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import os
 import re
 import openai
 from collections import deque
-from prompts import system_prompt, initial_prompts
+from prompts import system_prompt, initial_prompts, text_filters
 
 max_history = int(os.getenv("MAX_HISTORY", 10))
 max_tokens = int(os.getenv("MAX_TOKENS", 1024))
@@ -48,11 +48,21 @@ async def process_message(event, history, **kwargs):
 
     message = response["choices"][0]["message"]["content"]
 
-    history[event.chat_id].append(
-        {
-            "role": "assistant",
-            "content": message,
-        }
-    )
+    # Avoid appending unwanted text to history
+    no_record = False
+    for text_filter in text_filters:
+        if text_filter in message:
+            no_record = True
+            break
+
+    if no_record:
+        history[event.chat_id].pop()
+    else:
+        history[event.chat_id].append(
+            {
+                "role": "assistant",
+                "content": message,
+            }
+        )
 
     return message
