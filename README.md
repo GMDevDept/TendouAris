dd47a079ac91738ada3db275b539c6f0691d982d
PixelNeko <160456897+PxNeko@users.noreply.github.com>
2023-04-09 23:53:33 -0500

    Add welcome message, update readme

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
+<p align="center"><img width="400" src="resources/tendouaris.png" alt="Aris"></p>
+
 # TendouAris
+
+[![Telegram bot](https://img.shields.io/badge/bot-%40TendouArisBot-229ed9?logo=telegram&?style=for-the-badge)](https://t.me/TendouArisBot)
 
 ## Deployment
 
-1. ``` bash
+1. Download source code and go to project root directory
+
+    ``` bash
     git clone https://github.com/ToffeeNeko/TendouAris.git
-    cd TendouAris 
-   ```
+    cd TendouAris
+    ```
 
-2. rename .env.sample to .env and fill in the values
-3. rename prompts.sample.py to prompts.py and fill in the values
+2. Rename .env.sample to .env and fill in the values
+3. Rename prompts.sample.py to prompts.py and fill in the values
 
-4. ``` bash
+4. Build docker container
+
+    ``` bash
     docker-compose up -d --build
     docker image prune -f   # Remove unused dangling images (optional)
-   ```
+    ```
 
 ## Commands
 
@@ -21,17 +29,19 @@ You can copy the following commands and provide them to BotFather when building 
 
 1. Common commands
 
-   ``` md
+    ``` md
     aris - パンパカパーン！
     pop - 清除上次问答记忆，继续当前对话
     reset - 清除全部问答记忆，开始新的对话
-    apikey - 为当前对话添加OpenAI API key
+    apikey - 为当前会话添加OpenAI API key
+    chatid - 获取当前会话的chat ID
     version - 查看版本信息
-   ```
+    help - 爱丽丝食用指南
+    ```
 
 2. Admin commands
 
-   ``` md
-    fapikey - 为指定的对话添加API key
+    ``` md
+    fapikey - 为指定的会话添加API key
     fctrl - 为指定的群组启用/禁用刷屏限制
-   ```
+    ```
--- prompts.sample.py
+++ prompts.sample.py
@@ -14,15 +14,35 @@ initial_prompts = [
     },
 ]
 
-no_auth = (
-    "接触权限确认失败，爱丽丝无法回应对象的会话请求。\n\n请使用 `/apikey` 命令设置老师自己的OpenAI API key, 输入的密钥将仅供您一个人使用"
-)
+manual = """
+{} Sensei，欢迎您成为勇者爱丽丝的伙伴! 😆
+在出发冒险之前，记得请先使用 /apikey 指令设置爱丽丝的OpenAI API key哦~ 😉
+\n如何与爱丽丝对话:
+1. 私聊时，直接发送文字即可，也可以使用 /aris 指令 + 你的输入内容进行对话
+2. 在群聊中，可以使用 /aris 指令或回复爱丽丝发送的消息来与她对话。<b>当爱丽丝为群聊的管理员时</b>，她还可以自动识别以“爱丽丝”开头的消息并进行回复
+\nFAQ:
+Q: 爱丽丝忘记了自己的身份/拒绝回答问题/复读自己是语言模型怎么办?
+A: 尝试使用 /reset 指令重置对话
+Q: 为什么爱丽丝有时回复消息很慢?
+A: 爱丽丝的回复速度主要取决于OpenAI服务器的响应速度，服务器波动时就会出现回复慢甚至请求错误，不是爱丽丝本身的问题
+Q: 输入的API key安全吗?
+A: 私聊中输入的API key只会被用于你自己的帐号，群聊中输入的API key也只会被用于当前群聊，实在不放心欢迎自建
+\n爱丽丝指令集:
+/aris - パンパカパーン！
+/pop - 清除上次问答记忆，继续当前对话
+/reset - 清除全部问答记忆，开始新的对话
+/apikey - 为当前会话添加OpenAI API key
+/chatid - 获取当前会话的chat ID
+/version - 查看版本信息
+/help - 爱丽丝食用指南
+\n开源项目地址: <a href="https://github.com/ToffeeNeko/TendouAris">GitHub</a>
+"""
 
-api_key_set = "パンパカパーン! 爱丽丝的API Key已更新😎"
+no_auth = "接触权限确认失败，爱丽丝无法回应对象的会话请求。\n\n请使用 /apikey 命令设置老师自己的OpenAI API key, 输入的密钥将仅供当前会话及您自己使用"
 
-api_key_invalid = (
-    "抱歉老师, 爱丽丝无法验证您提供的API key。\n\n请按照正确格式输入自己的OpenAI API key: \n`/apikey sk-xxxxxxxxx`"
-)
+api_key_set = "パンパカパーン! 爱丽丝的API key已更新😎"
+
+api_key_invalid = "抱歉老师, 爱丽丝无法验证您提供的API密钥。\n\n请按照正确格式输入自己的<a href='https://platform.openai.com/account/api-keys'>OpenAI API key</a>: \n<code>/apikey sk-xxxxxxxxx</code>"
 
 flood_control_activated = "爱丽丝对话机能冷却中，机娘也是需要休息的！🥺\n\n(您在过去{}秒内的token用量 `[{}]` 已超过群内防刷屏阈值 `[{}]`，请稍候再试或私聊爱丽丝提供自己的API密钥)"
 
@@ -32,7 +52,7 @@ history_cleared = "好的老师，爱丽丝的记忆清理程序已启动。"
 
 last_message_cleared = "好的老师，爱丽丝已经忘掉我们的上一条对话啦！"
 
-api_error = "抱歉老师, 爱丽丝暂时无法完成API请求🥺"
+api_error = "抱歉老师, 爱丽丝暂时无法完成API请求🥲"
 
 no_record = "{}\n\n(由于{}, 爱丽丝不会保留本次会话的记忆)"
 
