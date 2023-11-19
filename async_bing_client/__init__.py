7054f62c24696b0dfe5454066e5dee2d9fc16222
UZQueen <53738915+HanaokaYuzu@users.noreply.github.com>
2023-11-19 00:37:06 -0600

    Use local async_bing_client dependency instead
    *Remove after merging PR

---
+++ async_bing_client/__init__.py
@@ -0,0 +1,14 @@
+from .client import Bing_Client
+from .const import ConversationStyle
+from .type import (
+    Notice,
+    Text,
+    Response,
+    Apology,
+    SuggestRely,
+    SourceAttribution,
+    SearchResult,
+    Image,
+    Limit,
+    NewChat,
+)
